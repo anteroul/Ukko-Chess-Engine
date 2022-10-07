f5a97341869c21b1f027ef2d1af3a881c8e60c59
Uljas Lindell <58420474+anteroul@users.noreply.github.com>
2022-10-07 17:24:36 +0300

    refactoring

--- src/Piece.hh
+++ src/Piece.hh
@@ -5,14 +5,13 @@ typedef enum { PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING, NONE } Type;
 typedef enum { BLACK, WHITE, UNDEFINED } Color;
 typedef enum { PLAYER, ENGINE, GHOST } User;
 
-typedef struct Piece
+struct Piece
 {
 	Type type;
 	Color color;
 	int x, y;
 	User user;
-} Piece;
-
+};
 
 
 #endif
